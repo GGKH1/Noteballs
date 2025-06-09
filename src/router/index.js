fc2f9ac690ee8362f3e5cc0933a2417e33c2c1d8
Gaga Khijakadze <xijakadzegaga@gmail.com>
2025-06-09 10:52:57 +0400

    feat: authentication page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import ViewNotes from '@/views/ViewNotes.vue'
 import ViewStats from '@/views/ViewStats.vue'
 import ViewEditNote from '@/views/ViewEditNote.vue'
+import ViewAuth from '@/views/ViewAuth.vue'
 
 const routes = [
     {
@@ -20,6 +21,11 @@ const routes = [
         name: 'stats',
         component: ViewStats
     },
+    {
+        path: '/auth',
+        name: 'auth',
+        component: ViewAuth
+    }
 ]
 
 const router = createRouter({
